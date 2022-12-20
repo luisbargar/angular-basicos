8dcec95e4a0d3dc9ade220747f25faddc7667abb
Luis Barreiros <luisbargar@gmail.com>
2022-12-20 14:16:44 +0100

    primer commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'app-contador',
+template: `
+
+        <h1> {{titulo}}</h1>
+        <h3>La base es : {{base}}</h3>
+        <button (click)="acumular(+1)">{{base}}</button>
+        <span>{{numero}}</span>
+        <button (click) = "acumular(-1)">-{{base}}</button>
+
+`
+
+})
+
+export class ContadorComponent{
+        titulo: string = 'Contador App';
+        numero: number = 10;
+        base: number = 5;
+        acumular(valor:number){
+    
+        this.numero += valor * this.base;
+        }
+
+}
---
+++ src/app/contador/contador/contador.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core";7
+import { ContadorComponent } from './contador.component';
+
+@NgModule({
+declarations:[
+ContadorComponent
+],
+exports:[
+    ContadorComponent
+]
+
+})
+export class ContadorModule{}
